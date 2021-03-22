b0e0042824bd1023427d62572a021ee8d4d0f23e
Jesse Ola <Jesse@Jesses-MacBook-Pro.local>
2021-03-22 21:17:53 +0000

    Added a basic login & logout flow

--- src/lib/schema/types/user.ts
+++ src/lib/schema/types/user.ts
@@ -14,6 +14,23 @@ export const RegisterUserInput = inputObjectType({
   },
 });
 
+export const LoginUserInput = inputObjectType({
+  name: 'LoginUserInput',
+  definition(t) {
+    t.string('email');
+    t.string('password');
+  },
+});
+
+export const LoginResponse = objectType({
+  name: 'LoginResponse',
+  definition(t) {
+    t.boolean('success');
+    t.field('user', { type: nullable('User') });
+  },
+});
+
+/* Main */
 export const User = objectType({
   name: 'User',
   definition(t) {
@@ -30,6 +47,18 @@ export const User = objectType({
 
 export const userQueries = queryType({
   definition(t) {
+    t.field('me', {
+      description: 'Returns the currently logged in user',
+      type: nullable('User'),
+      resolve: async (_, __, { req, prisma }) => {
+        const userId = req.session.userId;
+
+        if (!userId) return null;
+
+        return prisma.user.findUnique({ where: { id: userId } });
+      },
+    });
+
     t.field('users', {
       type: list('User'),
       args: {
@@ -68,7 +97,45 @@ export const userMutations = mutationType({
           },
         });
 
+        ctx.req.session.userId = newUser.id;
+
         return newUser;
+      },
+    });
+
+    t.field('loginUser', {
+      type: 'LoginResponse',
+      args: {
+        input: arg({ type: 'LoginUserInput' }),
+      },
+      resolve: async (_, { input }, ctx) => {
+        const { email, password } = input;
+        const user = await ctx.prisma.user.findUnique({ where: { email } });
+
+        if (!user) return { success: false, user: null };
+
+        const passwordsMatch = await argon.verify(user.password, password);
+
+        if (passwordsMatch) {
+          ctx.req.session.userId = user.id;
+          return { success: true, user };
+        } else {
+          return { success: false, user: null };
+        }
+      },
+    });
+
+    t.field('logoutUser', {
+      type: 'Boolean',
+      resolve: async (_, __, { req, res }) => {
+        return new Promise((resolve) => {
+          req.session.destroy((err: any) => {
+            if (err) resolve(false);
+
+            res.clearCookie('sid');
+            resolve(true);
+          });
+        });
       },
     });
   },
