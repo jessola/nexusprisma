d276dd8500d0ad90b2699d777ae554bcef1433ac
Jesse Ola <Jesse@Jesses-MacBook-Pro.local>
2021-03-26 20:35:16 +0000

    Removed the old @lib/schema directory

--- src/lib/schema/types/index.ts
+++
@@ -1,6 +0,0 @@
-/** All the types, queries, mutations & subscriptions should be exported here */
-export * from './utils';
-export * from './filteringInputs';
-
-export * from './user';
-export * from './userInputs';
--- src/lib/schema/types/utils/extensions.ts
+++
@@ -1,4 +0,0 @@
-import { asNexusMethod } from 'nexus';
-import { GraphQLDateTime } from 'graphql-iso-date';
-
-export const DateTime = asNexusMethod(GraphQLDateTime, 'date');
