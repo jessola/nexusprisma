d60a4edc18e35b0a6a3cb527f1fe8dd4427fe931
Jesse Ola <Jesse@Jesses-MacBook-Pro.local>
2021-03-26 20:23:28 +0000

    Fixed modules folder

--- src/lib/modules/accounts.ts
+++ src/lib/modules/accounts.ts
@@ -1,32 +1,47 @@
-import prisma from '@lib/db';
-import { Prisma } from '@prisma/client';
+import prisma, { Prisma, User } from '@lib/db';
 import argon from 'argon2';
+import { nanoid } from 'nanoid/async';
 
-type Credentials = { userId: number };
-
-async function registerUser(opts: Prisma.UserCreateInput) {
-  const user = await prisma.user.create({ data: opts });
-  return { data: { user } };
+/* CRUD */
+export function listUsers(opts?: Prisma.UserFindManyArgs) {
+  return prisma.user.findMany(opts);
 }
 
-async function loginUser(opts: { email: string; password: string }) {
-  const { email, password } = opts;
+export function getUser(opts: Prisma.UserFindUniqueArgs) {
+  return prisma.user.findUnique(opts);
+}
 
+/* Actions */
+export async function registerUser(opts: Prisma.UserCreateInput) {
+  const { password, ...otherFields } = opts;
+  const token = await createCsrfToken();
+  const newUser = await prisma.user.create({
+    data: { ...otherFields, password: await argon.hash(password) },
+  });
+
+  return { user: newUser, token };
+}
+
+export async function loginUser(opts: { email: string; password: string }) {
+  const { email, password } = opts;
   const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) return { success: false };
+
+  if (!user) return { user: null, token: null };
 
   const passwordsMatch = await argon.verify(user.password, password);
-  return passwordsMatch ? { success: true, user } : { success: false };
-}
-
-async function deleteUser(auth: Credentials) {
-  try {
-    await prisma.user.delete({ where: { id: auth.userId } });
-
-    return { deleted: true };
-  } catch (err) {
-    return { deleted: false };
+  if (passwordsMatch) {
+    const token = await createCsrfToken();
+    return { user, token };
   }
+
+  return { user: null, token: null };
 }
 
-export { registerUser, loginUser, deleteUser };
+/* Utilities */
+function createCsrfToken() {
+  return nanoid(32);
+}
+
+function createTokens(user: User) {}
+
+function verifyAccessToken(token: string) {}
